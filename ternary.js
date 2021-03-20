066e9069c083419e5405ca27601cfa5a52cd6ee4
Angella0 <angellasimbwa@gmail.com>
2021-03-20 06:47:34 +0300

    second attempt

---
+++ ternary.js
@@ -0,0 +1,25 @@
+ function getGrade(marks){
+var marks=89;
+var grade=59;
+if(marks>=80){
+    grade ='A';
+    console.log(grade);
+}else if(marks>=70){
+    grade='B';
+    console.log(grade);
+}else if(marks>=69){
+    grade='c';
+    console.log(grade);
+}else if(marks>=59){
+    grade='D';
+    console.log(grade);
+}else if(marks>=59){
+    grade='E';
+    console.log(grade);
+}else if(marks>=49){
+    grade='F';
+    console.log(grade);
+}
+}
+
+getGrade()
