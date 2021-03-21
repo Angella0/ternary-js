1b168730cb5fae0551ed8c6266cf7a2a433d482d
Angella0 <angellasimbwa@gmail.com>
2021-03-21 12:45:46 +0300

    second commit

--- conditional.js
+++ conditional.js
@@ -1,25 +1,26 @@
  function getGrade(marks){
-var marks=89;
-var grade=59;
+var marks=80;
+
 if(marks>=80){
     grade ='A';
-    console.log(grade);
-}else if(marks>=70){
-    grade='B';
+console.log(grade);
+}else if(marks>=79){
+    grade='B'
     console.log(grade);
 }else if(marks>=69){
     grade='c';
-    console.log(grade);
+   consol.log(grade);
 }else if(marks>=59){
     grade='D';
-    console.log(grade);
-}else if(marks>=59){
-    grade='E';
-    console.log(grade);
+   console.log(grade) ;
 }else if(marks>=49){
-    grade='F';
+    grade='E';
+   console.log(grade); 
+}else{
+    grade='anythingelse'
     console.log(grade);
 }
 }
 
 getGrade()
+ 
